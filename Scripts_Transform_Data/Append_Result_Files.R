1975abfcbf00c584d6a81c32d8b50aa6cad03afc
Bruno Cardoso <brsilvacardoso@gmail.com>
2024-02-10 16:10:41 +0000

    Creating scripts in R and saving the new append file

--- Scripts_Transform_Data/Append_Result_Files.R
+++ Scripts_Transform_Data/Append_Result_Files.R
@@ -1,6 +1,3 @@
-# Setting libraries -------------------------------------------------------
-library("writexl")
-
 # Setting directories -----------------------------------------------------
 
 #Checking the directory
@@ -40,21 +37,34 @@ View(races_2018_2023_appended)
 
 # Transform nc to order position ---------------------------
 
+# Converting the Pos column to numeric
+races_2018_2023_appended$Pos <-
+  as.integer(as.numeric(races_2018_2023_appended$Pos))
 
-races_2018_2023_appended$Pos <- as.integer(as.numeric(races_2018_2023_appended$Pos))
+#Checking missing values in Pos column
 nc_indices <- which(is.na(races_2018_2023_appended$Pos))
 
 
 for (idx in nc_indices) {
   if (idx > 1) {
-    races_2018_2023_appended$Pos[idx] <- races_2018_2023_appended$Pos[idx - 1] + 1
+    races_2018_2023_appended$Pos[idx] <-
+      races_2018_2023_appended$Pos[idx - 1] + 1
   }
 }
-races_2018_2023_appended$Pos <- ifelse(races_2018_2023_appended$Pos == 'NC', NA, races_2018_2023_appended$Pos)
-races_2018_2023_appended$Pos <- zoo::na.locf(races_2018_2023_appended$Pos, fromLast = FALSE)
+races_2018_2023_appended$Pos <-
+  ifelse(races_2018_2023_appended$Pos == 'NC',
+         NA,
+         races_2018_2023_appended$Pos)
+
+races_2018_2023_appended$Pos <-
+  zoo::na.locf(races_2018_2023_appended$Pos, fromLast = FALSE)
 
 
 View(races_2018_2023_appended)
 
 
 # Saving the dataframe ----------------------------------------------------
+
+write.csv(races_2018_2023_appended,
+          "./Raw_Files/races_2018_2023_appended.csv",
+          row.names = FALSE)
