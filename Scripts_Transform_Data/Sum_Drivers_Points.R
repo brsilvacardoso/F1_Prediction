e46ff88b37c7b3168d91e10f71a9399450a0f225
Bruno Cardoso <brsilvacardoso@gmail.com>
2024-02-11 11:38:04 +0000

    Creating script and file to sum drivers points

---
+++ Scripts_Transform_Data/Sum_Drivers_Points.R
@@ -0,0 +1,33 @@
+# Setting File ------------------------------------------------------------
+
+races <- read.csv("./Clean_Files/races_2018_2023_appended.csv")
+
+
+# Selecting Columns -------------------------------------------------------
+
+
+races <-
+  select(races, Driver_Name_Abbreviation,  PTS)
+
+View(races)
+
+
+# Summing Points ----------------------------------------------------------
+
+sum_points <-
+  tapply(races$PTS, races$Driver, FUN = sum, na.rm = TRUE)
+
+
+# Converting to dataframe
+sum_points_df <-
+  data.frame(Driver = names(sum_points),
+             Points = sum_points,
+             row.names = NULL)
+
+View(sum_points_df)
+
+
+# Saving File -------------------------------------------------------------
+
+write.csv(sum_points_df,
+          "./Clean_Files/sum_drivers_points.csv", row.names = FALSE)
